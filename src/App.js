5d47705fab4f99e7a5128042444569d86fa1a3aa
Camilo Romero <camids980@gmail.com>
2023-03-29 14:53:03 -0300

    Animation Added TO Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-
+import Movies from './componentes/Movies.jsx';
 function App() {
   return (
     <div className="conteiner">
         <header className="header">
-            <div className="play">
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-player-play" width="30" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M7 4v16l13 -8z" />
-                </svg>
+            <div className='cont-header'>
+                <div className="play">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-player-play" width="30" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M7 4v16l13 -8z" />
+                    </svg>
+                </div>
+                <h1 id="tittle">Movies Time</h1>
             </div>
-            <h1 id="tittle">Movies Time</h1>
         </header>
         <div className="separator"></div>
         <main className="main">
@@ -37,16 +39,7 @@ function App() {
                 </div>
             </section>
             <section className="cont-movies">
-                <ul className="movies">
-                    <li className="card">
-                        <h3 className="card-tittle">El Gato Con Botas</h3>
-                        <p className="card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo alias ab numquam tempora aliquam, at fugiat sequi magnam architecto perferendis harum? Autem similique corporis ipsa ipsum, enim porro velit sapiente.</p>
-                        <div className="card-options">
-                            <button className="card-button b-edit">Edit</button>
-                            <button className="card-button b-delete">Delete</button>
-                        </div>
-                    </li>
-                </ul>
+                <Movies/>
             </section>
         </main>
         <footer className="footer"></footer>
---
+++ src/componentes/Movies.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './movies.css';
+const Movies = () => {
+  return (
+    <ul className="movies">
+        <li className="card">
+            <h3 className="card-tittle">El Gato Con Botas</h3>
+            <p className="card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo alias ab numquam tempora aliquam, at fugiat sequi magnam architecto perferendis harum? Autem similique corporis ipsa ipsum, enim porro velit sapiente.</p>
+            <div className="card-options">
+                <button className="card-button b-edit">Edit</button>
+                <button className="card-button b-delete">Delete</button>
+            </div>
+        </li>
+    </ul>
+  )
+}
+
+export default Movies;
